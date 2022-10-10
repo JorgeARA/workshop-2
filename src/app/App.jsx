931bfdecfde3e802e79756f7c34d27834030fe56
camilon0 <kmilo8433@gmail.com>
2022-10-10 09:49:01 -0500

    nueva rama

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import Counter from './counter/Counter.jsx';
-import Form from './form/Form.jsx';
-import Test from './Test.jsx';
+import React, { Component } from "react";
+import Counter from "./counter/Counter.jsx";
+import Form from "./form/Form.jsx";
+import Test from "./Test.jsx";
 
 class App extends Component {
+  componentDidMount() {
+    console.log("Estoy iniciando");
+  }
 
-    componentDidMount(){
-        console.log('Estoy iniciando');
-    }
-
-    render(){
-        return (
-            <main>
-                <Test />
-                <h1>Hola Mundo</h1>
-                <section>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolorum inventore cum a voluptate. Dolorem tenetur, natus dolorum hic ratione adipisci cumque veritatis molestias nulla voluptatibus, quaerat placeat architecto quae.</p>
-                </section>
-                <Form />
-                <Counter />
-            </main>
-        )
-    }
+  render() {
+    return (
+      <main>
+        <Test />
+        <h1>Hola Mundo</h1>
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolorum
+            inventore cum a voluptate. Dolorem tenetur, natus dolorum hic
+            ratione adipisci cumque veritatis molestias nulla voluptatibus,
+            quaerat placeat architecto quae.
+          </p>
+          <a href="">Hola</a>
+        </section>
+        <Form />
+        <Counter />
+      </main>
+    );
+  }
 }
 
 export default App;
